52a6ffac436ab4238363704aa3054960f2ab7293
Laura  Rozo <lau@MacBook-Pro-de-Laura.local>
2023-07-21 06:34:14 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -2,6 +2,7 @@
 if ($_POST["module"] == "setValue") {
   $db = new Database();
   $breath = new Breath($db);
+  echo "string";exit;
   $breath->setStart($_POST["start"]);
   $breath->setJump( $_POST["jump"]);
   $breath->updateBreath();
