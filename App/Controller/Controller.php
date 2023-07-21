5ec76699b7f9a0d5d1393c4d4be97883904917d1
Laura  Rozo <lau@MacBook-Pro-de-Laura.local>
2023-07-21 06:48:53 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -1,8 +1,9 @@
 <?php
 require_once('../Config/database.php');
+echo "string";exit;
 require_once('../App/Models/Breath.php');
 if ($_POST["module"] == "setValue") {
-echo "string";exit;
+
   $db = new Database();
 
   $breath = new Breath($db);
