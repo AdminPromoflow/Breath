f7bccc973702ace92d161f4540a429304bcf058e
Laura  Rozo <lau@MacBook-Pro-de-Laura.local>
2023-07-25 13:57:36 -0500

    ...

--- App/Controller/Controller.php
+++ App/Controller/Controller.php
@@ -10,7 +10,7 @@ if ($_POST["module"] == "setValue") {
 
   $breath->setStart($_POST["start"]);
     $breath->setJump( $_POST["jump"]);
-    $breath->setIdUser( $_POST["jump"]);
+
     echo "string";exit;
   $breath->setIdUser('0');
 
